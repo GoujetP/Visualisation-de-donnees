f2177fc6db1d4758b1a07be22ae1156de3731e2a
Daniel Fanion <daniel.fanion.etu@univ-lille.fr>
2022-11-10 11:25:34 +0100

    fix

--- src/model/DManhattan.java
+++ src/model/DManhattan.java
@@ -6,11 +6,12 @@ public class DManhattan implements IDistance{
 
 	public double distance(IPoint p1, IPoint p2) {
 		double res = 0.0;
-		DataSet ds = p1.getDataset();
-		ds.get
+		//DataSet ds = p1.getDataset();
+		//ds.get
 		for(IColumn c : ) {
 				res += Math.abs(p1.getValue(c) - p2.getValue(c));
 		}
+		
 		return res;
 	}
 	
