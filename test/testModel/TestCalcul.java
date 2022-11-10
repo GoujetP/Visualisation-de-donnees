4d74aef3198a031a1d72cba15bef9e9b14b1b713
Daniel Fanion <daniel.fanion.etu@univ-lille.fr>
2022-11-10 09:30:38 +0100

    TDD Distance Manhattan + Euclidienne Début

--- test/testModel/TestCalcul.java
+++ test/testModel/TestCalcul.java
@@ -1,38 +1,42 @@
 package testModel;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 import model.DEuclidienne;
-import model.IColumn;
-import model.IDataset;
+import model.DManhattan;
 import model.IDistance;
-import model.IPoint;
-import model.IValueNormalizer;
 import model.Iris;
 
 public class TestCalcul {
-
+	static Iris i1 = new Iris(1.0,1.0,1.0,1.0);
+	static Iris i2 = new Iris(1.5,1.5,1.5,1.5);
+	static Iris i3 = new Iris(2.0,2.0,2.0,2.0);
+	static Iris i4 = new Iris(2.5,2.5,2.5,2.5);
+	
 	@Test
 	public void testGetNormalizedValue() {
-		Iris i1 = new Iris(1.0,1.0,1.0,1.0);
-		Iris i2 = new Iris(1.5,1.5,1.5,1.5);
-		Iris i3 = new Iris(2.0,2.0,2.0,2.0);
-		Iris i4 = new Iris(2.5,2.5,2.5,2.5);
+				
+	}
+	
+	@Test
+	public void testDistanceEuclidienne() {
+		IDistance d = new DEuclidienne();
+		double distanceValue = d.distance(i1, i2);
+		double distanceValue2 = d.distance(i1, i3);
 		
-		
-			
-			
+		assertEquals(distanceValue,1.0);
+		assertEquals(distanceValue2,2.0);
 	}
 	
 	@Test
 	public void testDistanceManhattan() {
-		Iris i1 = new Iris(1.0,1.0,1.0,1.0);
-		Iris i2 = new Iris(1.5,1.5,1.5,1.5);
-		
-		IDistance d = new DEuclidienne();
+		IDistance d = new DManhattan();
 		double distanceValue = d.distance(i1, i2);
+		double distanceValue2 = d.distance(i1, i3);
 		
-		assertEquals(distanceValue,??);
-		
+		assertEquals(distanceValue,2.0);
+		assertEquals(distanceValue2,4.0);
 	}
 }
