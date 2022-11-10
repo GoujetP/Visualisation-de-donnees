8eaa880234e1e1bc44714410570e9f6c2556f957
Pierre Goujet <pierre.goujet.etu@univ-lille.fr>
2022-11-10 09:09:45 +0100

    utils subject observer

---
+++ src/utils/Observer.java
@@ -0,0 +1,9 @@
+package utils;
+
+
+public interface Observer {
+	public void update(Subject subj);
+
+	public void update(Subject subj, Object data);
+}
+
--- src/view/PointView.java
+++ src/view/PointView.java
@@ -1,5 +1,20 @@
 package view;
 
+import utils.Observer;
+import utils.Subject;
+
 public class PointView implements Observer{
+
+	@Override
+	public void update(Subject subj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Subject subj, Object data) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
